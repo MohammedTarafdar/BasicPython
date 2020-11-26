b74dcf00954b4622a648cd044796c91ce283e913
MohammedTarafdar <mtarafdar96@gmail.com>
2020-11-25 22:56:27 -0500

    first commit

---
+++ operatorsExample/ArithmeticOperators.py
@@ -0,0 +1,81 @@
+
+"""
+Arithmetic Operators are as follows:
+"+" => addition
+
+"""
+num1 = 6
+num2 = 7
+num3 = 20
+num4 = 5
+num5 = 3
+num6 =2
+
+Total = num1+num2
+
+print( Total)
+print("*****************")
+
+"""
+"-" => subtraction
+"""
+
+
+sum_of_two_number = num3 - num2
+print(sum_of_two_number)
+print("*****************")
+
+"""
+" * " => multiplication
+"""
+multi_of_two_number = num1 * num2
+print(multi_of_two_number)
+print("*****************")
+
+"""
+" / " => division
+"""
+
+
+division1_of_two_number = num3 / num4
+division2_of_two_number = num2 / num5
+
+print(division1_of_two_number)
+print(division2_of_two_number)
+print("*****************")
+
+
+"""
+" % " => reminder 
+"""
+
+rem_of_two_number = num1 % num4
+print(rem_of_two_number)
+print("*****************")
+
+"""
+" // " => floor division
+"""
+
+
+div1 = num2 / num5
+print(div1)
+print("*****************")
+
+div2 = num2 // num5
+print(div2)
+print("*****************")
+
+
+"""
+" ** " => exponent
+"""
+
+
+
+exponent_value1 = num5 ** num6
+
+exponent_value2 = num1 ** num6
+
+print(exponent_value1)
+print(exponent_value2)
